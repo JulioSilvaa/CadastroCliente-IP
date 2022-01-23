392c8961bb42d7b61e31d18e8e6b011ed6850d9d
juliosan-bit <julioesar.amancio@yahoo.com.br>
2022-01-23 17:57:04 -0300

    realizando a entrega do projeto

--- src/Algoritmos/Algoritmos.js
+++ src/Algoritmos/Algoritmos.js
@@ -1,5 +1,4 @@
-
-
+/* eslint-disable no-undef */
 // 1 -  fibonacci.js
 const fibonacci = (n) => {
   let fq = [0, 1];
@@ -19,5 +18,48 @@ const fibonacci = (n) => {
 
 console.log(fibonacci(6));
 
+// Exercício 3
 
+// const newObj = (obj) => {
+//   for (const property in obj) {
+//     if (obj[property] !== null) {
+//       console.log({ [property]: obj[property] });
+//     }
+//   }
+// };
+// console.log(
+//   newObj({
+//     fizz: "buzz",
+//     foo: null,
+//     bar: 42,
+//   })
+// );
 
+// const obj = {
+//   fizz: "buzz",
+//   foo: null,
+//   bar: 42,
+// };
+
+// Object.keys(obj).forEach(function (element) {
+//   if (obj[element] !== null) {
+//     console.log({ [element]: obj[element] });
+//   }
+// });
+
+const validation = (date) => {
+  const now = Date.now();
+  const past = Date.parse(date)
+  const duraction = now - past;
+
+  console.log(now,new Date(now),"now");
+  console.log(past,new Date(past),"past");
+ 
+
+  console.log(new Date(duraction),"resultado");
+};
+
+validation("2021-11-17T20:40:09.503Z", "10d");
+
+//("2021-12-10T00:00:00.000Z", "180d");
+//"2021-11-17T20:40:09.503Z", "10d"
